d93ee099a1b50cfa7ccf00b899bde14cec32cb53
LeBarbu31 <f.mengue31@gmail.com>
2022-07-17 15:21:50 +0200

    move addTask on homepage

--- src/components/addTask/AddTask.js
+++ src/components/addTask/AddTask.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addTask } from '../../redux/action/task.action';
 
 export default function AddTask() {
+    const [taskTitle, setTaskTitle] = useState('');
+
+    const dispatch = useDispatch();
+
+    const handleClickSave = () => {
+        dispatch(addTask(taskTitle, ''))
+    };
+
     return (
-        <div>AddTask</div>
+        <div id="form_input">
+            <input
+                class="standard-input"
+                type="text"
+                placeholder="Add a task."
+                onChange={(e) => setTaskTitle(e.target.value)} />
+            <button
+                class="todo-btn standard-button"
+                type="submit"
+                onClick={() => handleClickSave()}>
+                Add Task
+            </button>
+        </div>
     )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,28 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import TodoCard from '../../components/todoCard/TodoCard';
-import { getTasks } from '../../redux/action/task.action';
-import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import ModalInput from '../../components/modalInput/ModalInput';
+import AddTask from '../../components/addTask/AddTask';
 
 export default function Home() {
 
-    const [addTaskShow, setAddTaskShow] = useState(false);
     const todoList = useSelector((state) => state.taskReducer);
-
-    const showImputModal = () => {
-        setAddTaskShow(!addTaskShow)
-    }
 
     return (
         <div>
-            <h1 className="page-title">My Todo list</h1>
-            {addTaskShow && <ModalInput showState={() => showImputModal()} />}
-            {
-                todoList.map((task, key) => (
-                    <TodoCard task={task} key={key} />
-                ))
-            }
+            <header id="header">
+                <h1 className="page-title">My Todo list</h1>
+            </header>
+
+            <AddTask />
+            <div className="todo_list">
+                <div className="todo_list_order open">
+                    {
+                        todoList.map((task, key) => (
+                            <Fragment key={key}>
+                                {(!task.doneState) && (
+                                    <TodoCard task={task} key={key} />
+                                )}
+                            </Fragment>
+                        ))
+                    }
+
+                </div>
+                <div className="todo_list_order close">
+                    {
+                        todoList.map((task, key) => (
+                            <Fragment key={key}>
+                                {(task.doneState) && (
+                                    <TodoCard task={task} key={key} />
+                                )}
+                            </Fragment>
+                        ))
+                    }
+                </div>
+            </div>
+
         </div >
     )
 }
